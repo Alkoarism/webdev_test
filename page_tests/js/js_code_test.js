e965a94f608883402bca26ce836d5abd25b2cd26
alkoarism <69710741+Alkoarism@users.noreply.github.com>
2023-11-10 12:52:38 -0300

    EXERCISE: Lots of exercises about js fundamentals

--- page_tests/js/js_code_test.js
+++ page_tests/js/js_code_test.js
@@ -37,7 +37,7 @@ console.log(typeof myFloat);
 const lotsOfDecimals = 1.15613183153831;
 console.log(lotsOfDecimals);
 const twoDecimalPlaces = lotsOfDecimals.toFixed(2);
-console.log(twoDecimalPlaces)
+console.log(twoDecimalPlaces);
 
 //Stringo to number conversion
 let myNumber = "74";
@@ -67,7 +67,7 @@ console.log(NaN ** 0); //Of all the math rules NaN could refuse to obey they cho
 const someone = "Carlos";
 console.log(`Hi's name is ${someone}!`);
 console.log(`Well, this is some funky quote... ${1 + "Awooga!"}`);
-console.log(`Now, for a funkier quote: ${null + NaN + "Funky" + Infinity + true}`)
+console.log(`Now, for a funkier quote: ${null + NaN + "Funky" + Infinity + true}`);
 
 console.log(typeof 0, 
     typeof 14n, 
@@ -78,3 +78,27 @@ console.log(typeof 0,
     typeof alert, //This is also wrong, being technically a special object called function
     typeof Math);
 
+console.log("Day 3 of learning this wondrous language.");
+
+//some crazy logical operations in js:
+console.log(true||"Not printed");
+console.log(false||"Printed"); //or condition is not limited to boolean
+
+if(1 && 0){
+    alert("This won't be printed");
+} else{
+    console.log("This, however, will be printed.");
+}
+
+/*The first alert will be shown as it needs to be evaluated before
+    conversion to boolean and thus, change to undefined, 2 will be
+    evaluated as true as son, the comparisons halt*/
+//alert(alert(1) || 2 || alert(3));
+
+if(""){
+    console.log("Empty string");
+} else if (" " == "   "){
+    console.log("Single spaced string");
+} else{
+    console.log("No empty space");
+}
--- page_tests/js/js_code_test_2.js
+++ page_tests/js/js_code_test_2.js
@@ -1,5 +1,6 @@
 //Page item manipulation script:
 
+//JS and HTML communication test
 const btn = document.querySelector(".thing-doer");
 const txt = document.querySelector(".button-guess");
 
@@ -15,6 +16,7 @@ function updateBtn(){
     }
 }
 
+//Butotn and conditional testing
 const inquirer = document.querySelector(".inquirer");
 
 function inquiry(){
@@ -26,5 +28,50 @@ function inquiry(){
         answer.textContent = "We do not appreciate your kind here sir.";
     }
 }
-
 inquirer.addEventListener("click", inquiry);
+
+//Age testing
+const ageCheck = document.querySelector(".age-check");
+ageCheck.addEventListener("click", ageTesting);
+
+function ageTesting(){
+    const result = document.querySelector(".result");
+    result.textContent = "Result: ";
+    const check = prompt("Enter your age, please");
+    const numericalCheck = Number(check);
+    console.log(numericalCheck);
+
+    if (!numericalCheck){
+        result.textContent += "Enter a valid number, please";
+    } else if(numericalCheck < 14 || numericalCheck > 90){
+        result.textContent += "You are outside of the allowed age range.";
+    } else{
+        result.textContent += "You're good to go";
+    }
+}
+
+//Password checking
+const loginButton = document.querySelector(".login button");
+loginButton.addEventListener("mouseup", login);
+
+function login(){
+    const logName = prompt("Login name:");
+    const logOutput = document.querySelector(".log-result");
+    logOutput.textContent = "Log Result: ";
+    console.log(typeof(logName));
+
+    if(!logName || logName === ""){
+        logOutput.textContent += "Canceled";
+    } else if(logName === "Admin"){
+        const logPassword = prompt("Password:");
+        if(!logPassword || logPassword === ""){
+            logOutput.textContent += "Canceled";
+        } else if(logPassword === "TheMaster") {
+            logOutput.textContent += "Welcome!";
+        } else{
+            logOutput.textContent += "Wrong password"
+        }
+    } else{
+        logOutput.textContent += "I don't know you.";
+    }
+}
