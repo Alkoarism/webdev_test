b5430f9fb2ca1488203c9157089b9753188d07a8
alkoarism <69710741+Alkoarism@users.noreply.github.com>
2023-12-07 05:52:15 -0300

    UPDATE: Add snake game javascript pseudocode

--- snake_game/js/snake_game.js
+++ snake_game/js/snake_game.js
@@ -1,3 +1,31 @@
-const p = document.querySelector("p");
+const gameWindow = document.querySelector("main");
+const gameContainer = document.querySelector("#game-container");
 
-p.textContent = "Something to test this page's javascript script";
+let Snake = {
+    posH: 0,
+    posV: 0,
+    size: 2,
+    /*sprites:{
+        head:"",
+        body:"",
+        tail:"",
+    },*/
+    direction:{
+        dirH: 1,
+        dirV: 1,
+    }
+}
+
+function createBoard(width, height){}
+
+function positionSnake(boardWidth, boardHeight){}
+
+function changeSnakeDirection(direction){}
+
+function moveSnake(boardWidht, boardHeight){}
+
+function gameInit(screenWidth, screenHeight){
+    createBoard();
+    positionSnake();
+}
+
