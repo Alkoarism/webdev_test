8cb87f91be9f7a28f1ede8de54df73e83ce25397
alkoarism <69710741+Alkoarism@users.noreply.github.com>
2024-03-04 00:04:45 -0300

    FIX: Updated simple operations functionality

--- calculator_project/js/calculator_core.js
+++ calculator_project/js/calculator_core.js
@@ -3,7 +3,7 @@ const operations = new Map();
 
 operations.set('+', add);
 operations.set('-', subtract);
-operations.set('*', multiply);
+operations.set('x', multiply);
 operations.set('/', divide);
 operations.set('%', percentage);
 
@@ -15,32 +15,51 @@ RESULT = {
 
 //----------------- System related functions ----------------------------------
 function resolve(text){
-    RESULT.value = solveEquation(text);
-    console.log(RESULT.value);
+    if (text === ''){RESULT.solved = false;}
+    else {RESULT.value = solveOperation(text);}
 
     if (RESULT.solved){
         return RESULT.value;
-    } else
+    } else{
+        RESULT.solved = true;
         return RESULT.error;
-}
-
-function solveEquation(input){
-    if (input.search(/\(/) !== -1) { return solveParenthesis(input);}
-
-
+    }
 }
 
 function solveParenthesis(parenthesized){
-    
+
 }
 
-function solveOperation(a, b, operator){
+function solveOperation(operation){
+    if (operation.search(/\(/) !== -1) { return solveParenthesis(operation);}
+
+    const operatorPos = operation.search(/\x|\/|\+|\-|\%/);
+    if (operatorPos === -1){return parseFloat(operation);}
+    if (operation[operatorPos] === '%'){
+        return operate(
+            operation.slice(0, operatorPos), 
+            0, 
+            operation[operatorPos]);
+    }
+    if (operatorPos === operation.length - 1){return '';}
+    if (operation[operatorPos] === '/' && parseFloat(operation.slice(operatorPos + 1)) === 0){
+        RESULT.error = 'DIVISION BY 0';
+        RESULT.solved = false;
+        return 0;
+    }
+
+    return operate(
+        parseFloat(operation.slice(0, operatorPos)),
+        parseFloat(operation.slice(operatorPos + 1)),
+        operation[operatorPos]);
+}
+
+function operate(a, b, operator){
     if (operator === '%'){ return operations.get(operator)(a);}
     else {return operations.get(operator)(a, b);}
 }
 
-
-//----------------- Four calculator -------------------------------------------
+//----------------- Operations ------------------------------------------------
 function add(a, b){
     return a + b;
 }
